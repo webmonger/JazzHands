68db2714bb3f90c63bc3311ee65d8b18d3a927b8
Jon Carlos <jon@screenmedia.co.uk>
2014-06-26 14:34:05 +0100

    Fixed issue with frame and size not being setup properly

--- JazzHandsCSharp/Screenmedia.IFTTT.JazzHands/FrameAnimation.cs
+++ JazzHandsCSharp/Screenmedia.IFTTT.JazzHands/FrameAnimation.cs
@@ -7,48 +7,49 @@ using MonoTouch.CoreGraphics;
 
 namespace Screenmedia.IFTTT.JazzHands
 {
-	public class FrameAnimation : Animation
+    public class FrameAnimation : Animation
     {
-		public void Animate(int time)
-		{
-			if (KeyFrames.Count () <= 1)
-				return;
+        public void Animate(int time)
+        {
+            if (KeyFrames.Count() <= 1)
+                return;
 
-			AnimationFrame animationFrame = AnimationFrameForTime(time);
+            AnimationFrame animationFrame = AnimationFrameForTime(time);
 
-			// Store the current transform
-			CGAffineTransform tempTransform = View.Transform;
+            // Store the current transform
+            CGAffineTransform tempTransform = View.Transform;
 
-			// Reset rotation to 0 to avoid warping
-			View.Transform = CGAffineTransform.MakeRotation(0);
-			View.Frame = animationFrame.Frame;
+            // Reset rotation to 0 to avoid warping
+            View.Transform = CGAffineTransform.MakeRotation(0);
+            View.Frame = animationFrame.Frame;
 
-			// Return to original transform
-			View.Transform = tempTransform;
-		}
+            // Return to original transform
+            View.Transform = tempTransform;
+        }
 
-		public AnimationFrame FrameForTime (int time,
-			AnimationKeyFrame startKeyFrame,
-			AnimationKeyFrame endKeyFrame)
-		{
-			int startTime = startKeyFrame.Time;
-			int endTime = endKeyFrame.Time;
-			RectangleF startLocation = startKeyFrame.Frame;
-			RectangleF endLocation = endKeyFrame.Frame;
+        public AnimationFrame FrameForTime(int time,
+            AnimationKeyFrame startKeyFrame,
+            AnimationKeyFrame endKeyFrame)
+        {
+            int startTime = startKeyFrame.Time;
+            int endTime = endKeyFrame.Time;
+            RectangleF startLocation = startKeyFrame.Frame;
+            RectangleF endLocation = endKeyFrame.Frame;
 
-			RectangleF frame = View.Frame;
-			frame.Location.X = TweenValueForStartTime(startTime, endTime, startLocation.GetMinX, endLocation.GetMinX, time);
-			frame.Location.Y = TweenValueForStartTime(startTime, endTime, startLocation.GetMinY, endLocation.GetMinY, time);
-			frame.Size.Width = TweenValueForStartTime (startTime, endTime, startLocation.Width, endLocation.Width, time);// ? : 0;
-			frame.Size.Height = TweenValueForStartTime(startTime, endTime, startLocation.Height, endLocation.Height, time);// ? : 0;
+            RectangleF frame = View.Frame;
+            frame.Location =
+                new PointF(
+                    TweenValueForStartTime(startTime, endTime, startLocation.GetMinX(), endLocation.GetMinX(), time),
+                    TweenValueForStartTime(startTime, endTime, startLocation.GetMinY(), endLocation.GetMinY(), time));
+            frame.Size =
+                new SizeF(TweenValueForStartTime(startTime, endTime, startLocation.Width, endLocation.Width, time),
+                    TweenValueForStartTime(startTime, endTime, startLocation.Height, endLocation.Height, time));
 
-			AnimationFrame animationFrame = new AnimationFrame();
-			animationFrame.Frame = frame;
+            AnimationFrame animationFrame = new AnimationFrame();
+            animationFrame.Frame = frame;
 
-			return animationFrame;
-		}
-
-
+            return animationFrame;
+        }
     }
 }
 
